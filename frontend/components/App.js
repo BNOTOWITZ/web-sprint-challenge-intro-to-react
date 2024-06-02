c16c489ccd32196576ac8669d4b97471e762db41
Brandon Notowitz <151097606+BNOTOWITZ@users.noreply.github.com>
2024-06-02 19:12:11 -0400

    Update App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,45 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Character from './Character';
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Character from './Character'
 
-const urlPlanets = 'http://localhost:9009/api/planets';
-const urlPeople = 'http://localhost:9009/api/people';
+const urlPlanets = 'http://localhost:9009/api/planets'
+const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
+
   const [characters, setCharacters] = useState([]);
 
-  // ❗ Create effects to fetch the data and put it in state
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const peopleResponse = await axios.get(urlPeople);
-        const planetsResponse = await axios.get(urlPlanets);
-
-        const peopleWithPlanets = peopleResponse.data.map((person) => ({
-          ...person,
-          homeworld: planetsResponse.data.find((planet) => planet.id === person.homeworld_id),
-        }));
-
-        setCharacters(peopleWithPlanets);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const fetchData = () => {
+    let requestPlanets = axios.get(urlPlanets);
+    let requestPeople = axios.get(urlPeople);
+    let Datas = [];
+    Promise.all([requestPlanets, requestPeople])
+      .then(responses => {
+        const [responsePlanets, responsePeople] = responses;
+        responsePeople.data.forEach((data1) => {
+          responsePlanets.data.forEach((data2) => {
+            if (data1.homeworld === data2.id)
+            {
+              let newData = data1;
+              newData.homeworld = data2;
+              Datas.push(newData);
+            }
+          })
+        })
+        setCharacters(Datas);
+      })
+      .catch(error => {
+          console.log('Error: ', error);
+      });
+  }
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
+      {/* <p>See the README of the project for instructions on completing this challenge</p> */}
+      {
+        characters.map(
+          (character, index) => <Character key={index} character={character}/>
+        )
+      }
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
-      {characters.map((character) => (
-        <Character key={character.id} character={character} />
-      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// ❗ DO NOT CHANGE THE CODE  BELOW
+if (typeof module !== 'undefined' && module.exports) module.exports = App
